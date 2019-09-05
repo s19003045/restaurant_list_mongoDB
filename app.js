9e0036e4b856e9e4c7a960d67df956cc1cfc7d52
Gary Wu <s19003045@gmail.com>
2019-09-05 11:38:19 +0800

    Add index page and action

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// import mongoose and connect to mongoDB
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
+const db = mongoose.connection
+
+// import mongoose model
+const Restaurant = require('./models/restaurant')
+
+// actions if connect error
+db.on('err', (err) => {
+  if (err) return console.error(err)
+})
+
+// actions if connect success
+db.once('open', (err) => {
+  if (err) return console.error(err)
+  console.log('connect to mongoDB successifully !')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs('defaultLayout: main'))
@@ -18,11 +36,15 @@ app.use(express.static('public'))
 
 // review 所有餐廳：GET / 或 GET / restaurants
 app.get('/', (req, res) => {
-  res.send('review 所有餐廳')
+  // res.send('review 所有餐廳')
+  Restaurant.find((err, restaurants) => {
+    if (err) return console.error(err)
+    res.render('index', { restaurants: restaurants })
+  })
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('review 所有餐廳')
+  res.redirect('/')
 })
 
 // review 單一餐廳：GET / restaurants /: id
