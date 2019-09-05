296bb10ba8a8a99c74393f1b4f2c1f5b12bcb6b5
Gary Wu <s19003045@gmail.com>
2019-09-05 11:00:00 +0800

    Add restaurant Seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,3 +1,9 @@
+// import mongoose Schema
+const Restaurant = require('../restaurant')
+
+// import restaurant.json
+const restaurantsList = require('./restaurant.json')
+
 // import mongoose and connect to mongoDB
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
@@ -13,3 +19,21 @@ db.once('open', (err) => {
   if (err) return console.error(err)
   console.log('connect to mongoDB successifully !')
 })
+
+// 迭代每一筆餐廳資料，以 Restaurant(mongoose Schema 之實例來儲存至 mongoDB)
+restaurantsList.results.forEach(element => {
+  Restaurant.create({
+    name: element.name,
+    name_en: element.name_en,
+    category: element.category,
+    image: element.image,
+    location: element.location,
+    phone: element.phone,
+    google_map: element.google_map,
+    rating: element.rating,
+    description: element.description
+  }, (err, restaurants) => {
+    if (err) return console.error(err)
+
+  })
+});
